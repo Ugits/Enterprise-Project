010c197897b2c5123235a745dea0dd85568c7221
Jonas Ullvahn <ullvahn@gmail.com>
2024-11-06 19:45:10 +0100

    initial commit

---
+++ src/main/java/org/jonas/enterpriseproject/EnterpriseProjectApplication.java
@@ -0,0 +1,13 @@
+package org.jonas.enterpriseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jonas/enterpriseproject/EnterpriseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jonas.enterpriseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
