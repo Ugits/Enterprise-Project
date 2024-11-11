527dcb0bed4181df887334001e047e6738e0ee73
Jonas Ullvahn <ullvahn@gmail.com>
2024-11-11 13:28:17 +0100

    Add CustomUserDetails and Service

---
+++ src/main/java/org/jonas/enterpriseproject/config/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package org.jonas.enterpriseproject.config.security;
+
+import org.jonas.enterpriseproject.user.model.entity.CustomUser;
+import org.jonas.enterpriseproject.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        CustomUser customUser = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new CustomUserDetails(customUser);
+    }
+}
--- src/main/java/org/jonas/enterpriseproject/user/repository/UserRepository.java
+++ src/main/java/org/jonas/enterpriseproject/user/repository/UserRepository.java
@@ -4,6 +4,11 @@ import org.jonas.enterpriseproject.user.model.entity.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<CustomUser, Long> {
+
+    Optional<CustomUser> findByUsername(String username);
+
 }
