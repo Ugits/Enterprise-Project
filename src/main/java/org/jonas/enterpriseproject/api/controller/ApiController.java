23f97ba0ae2d80ab12797c1bf7e5518ef226612d
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-01 16:49:09 +0100

    Add Api controller and service

---
+++ src/main/java/org/jonas/enterpriseproject/api/controller/ApiController.java
@@ -0,0 +1,11 @@
+package org.jonas.enterpriseproject.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+
+}
---
+++ src/main/java/org/jonas/enterpriseproject/api/service/ApiService.java
@@ -0,0 +1,8 @@
+package org.jonas.enterpriseproject.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+}
