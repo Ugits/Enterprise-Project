48e3c278235a676922aac7dcd7c5167a6016ab0b
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-01 17:07:04 +0100

    Add WebClient Bean

---
+++ src/main/java/org/jonas/enterpriseproject/api/config/ApiWebClient.java
@@ -0,0 +1,21 @@
+package org.jonas.enterpriseproject.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class ApiWebClient {
+
+    @Value("${API_URL}")
+    private String apiUrl;
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient
+                .builder()
+                .baseUrl(apiUrl);
+    }
+
+}
